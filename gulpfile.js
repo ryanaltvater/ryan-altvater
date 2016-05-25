0a0a2e59b0fce512fa626dea7cc891de32a5e8f2
Ryan Altvater <ryan@ryanaltvater.com>
2016-05-25 16:05:28 -0400

    moved dependency files and dotfiles to project root

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,76 @@
 /* ============================== */
-/*  GULP
+/*  Gulp
 /* ============================== */
 
 var gulp = require('gulp');
 
 
 /* ============================== */
-/*  PLUGINS
+/*  Plugins
 /* ============================== */
 
-var autoprefixer 		= require('gulp-autoprefixer'),
-	bower 				= require('./bower.json'),
-	browserSync 		= require('browser-sync').create(),
-	changed 			= require('gulp-changed'),
-	gcmq 				= require('gulp-group-css-media-queries'),
-	concat 				= require('gulp-concat'),
-	cssmin 				= require('gulp-cssmin'),
-	fileinclude			= require('gulp-file-include'),
-	header 				= require('gulp-header'),
-	iconfont 			= require('gulp-iconfont'),
-	notify 				= require('gulp-notify'),
-	package 			= require('./package.json'),
-	plumber 			= require('gulp-plumber'),
-	reload 				= browserSync.reload,
-	replace 			= require('gulp-replace'),
-	runSequence 		= require('run-sequence'),
-	sass 				= require('gulp-sass'),
-	uglify 				= require('gulp-uglify');
-
-
-/* ============================== */
-/*  DESTINATIONS
-/* ============================== */
-
-var srcRoot 			= './',
-    srcSCSS 			= 'assets/css/scss/',
-    srcCSS 				= 'assets/css/',
-    srcFonts 			= 'assets/fonts/',
-    srcImg 				= 'assets/img/',
-    srcIcons 			= 'assets/img/svg/',
-    srcJS 				= 'assets/js/',
-    srcTemplates 		= 'templates/',
-	destRoot 			= '../public/',
-	destCSS 			= '../public/assets/css/',
-    destFonts 			= '../public/assets/fonts/',
-    destImg 			= '../public/assets/img/',
-    destJS 				= '../public/assets/js/';
+var autoprefixer = require('gulp-autoprefixer'),
+	bower = require('./bower.json'),
+	browserSync = require('browser-sync').create(),
+	changed = require('gulp-changed'),
+	concat = require('gulp-concat'),
+	cssmin = require('gulp-cssmin'),
+	fileinclude = require('gulp-file-include'),
+	gcmq = require('gulp-group-css-media-queries'),
+	header = require('gulp-header'),
+	iconfont = require('gulp-iconfont'),
+	notify = require('gulp-notify'),
+	package = require('./package.json'),
+	plumber = require('gulp-plumber'),
+	reload = browserSync.reload,
+	replace = require('gulp-replace'),
+	runSequence = require('run-sequence'),
+	sass = require('gulp-sass'),
+	uglify = require('gulp-uglify');
 
 
 /* ============================== */
-/*  BANNER
+/*  Locations
+/* ============================== */
+
+var srcRoot = 'src/',
+    srcSCSS = 'src/assets/css/scss/',
+    srcCSS = 'src/assets/css/',
+    srcFonts = 'src/assets/fonts/',
+    srcImg = 'src/assets/img/',
+    srcIcons = 'src/assets/img/svg/',
+    srcJS = 'src/assets/js/',
+    srcTemplates = 'src/templates/',
+	destRoot = 'public/',
+	destCSS = 'public/assets/css/',
+    destFonts = 'public/assets/fonts/',
+    destImg = 'public/assets/img/',
+    destJS = 'public/assets/js/';
+
+
+/* ============================== */
+/*  Banner
 /* ============================== */
 
 var banner = [
-	'/* ============================== */\n' +
+	'/* ========================================================================================== */\n' +
 	'/*  <%= package.name %>\n' +
 	'/*  <%= package.description %>\n' +
 	'/*  \n' +
-	'/*  Author: <%= package.author %>\n' +
-	'/*  URL: <%= package.url %>\n' +
-	'/* ============================== */\n\n'
+	'/*  Copyright © ' + new Date().getFullYear() + '. Released under the <%= package.license %> license.\n' +
+	'/*  <%= package.homepage %>\n' +
+	'/* ========================================================================================== */\n\n'
 ].join('\n');
 
 
 /* ============================== */
-/*  FILE INCLUDE
+/*  File Include
 /* ============================== */
 
 gulp.task('fileinclude', function() {
 	return gulp.src([
-		srcRoot 		+ '**/*.html',
-		'!' + srcRoot 	+ 'bower_components/**/*',
-		'!' + srcRoot 	+ 'node_modules/**/*',
-		'!' + srcRoot 	+ 'templates/**/*'
+		srcRoot + '**/*.html',
+		'!' + srcRoot + 'templates/**/*'
 	])
 	.pipe(fileinclude({
 		prefix: '@@',
@@ -99,79 +97,79 @@ gulp.task('html', function() {
 
 gulp.task('css', function() {
 	return gulp.src([
-		srcSCSS 		+ 'styles.scss'
+		srcSCSS + 'styles.scss'
 	])
 	.pipe(plumber({
-		errorHandler	: notify.onError('Error: <%= error.message %>')
+		errorHandler: notify.onError('Error: <%= error.message %>')
 	}))
 	.pipe(sass())
 	.pipe(autoprefixer({
-		browsers		: ['last 2 versions'],
-		cascade			: false
+		browsers: ['last 2 versions'],
+		cascade: false
 	}))
 	.pipe(gcmq())
 	.pipe(cssmin())
 	.pipe(header(banner, {
-		package			: package
+		package: package
 	}))
 	.pipe(gulp.dest(destCSS))
 	.pipe(reload({
-		stream			: true
+		stream: true
 	}));
 });
 
 
 /* ============================== */
-/*  FONTS
+/*  Fonts
 /* ============================== */
 
 gulp.task('fonts', ['icons'], function() {
 	return gulp.src([
-		srcFonts 		+ '**/*'
+		srcFonts + '**/*'
 	])
 	.pipe(changed(destFonts))
 	.pipe(gulp.dest(destFonts))
 	.pipe(reload({
-		stream			: true
+		stream: true
 	}));
 });
 
 
 /* ============================== */
-/*  IMAGES
+/*  Images
 /* ============================== */
 
 gulp.task('images', function() {
 	return gulp.src([
-		srcImg 			+ '**/*',
+		srcImg + '**/*',
 	    '!' + srcIcons,
 	    '!' + srcIcons 	+ '**/*'
 	])
 	.pipe(changed(destImg))
 	.pipe(gulp.dest(destImg))
 	.pipe(reload({
-		stream			: true
+		stream: true
 	}));
 });
 
 
 /* ============================== */
-/*  ICONS
+/*  Icons
 /* ============================== */
 
 gulp.task('icons', function() {
 	return gulp.src([
-		srcIcons 		+ '*.svg'
+		srcIcons + '*.svg'
 	])
 	.pipe(iconfont({
-		fontName		: 'icons',
-		normalize		: true,
+		fontName: 'icons',
+		normalize: true,
 		appendCodepoints: true
-//		appendUnicode	: true
+//		appendUnicode: true
 	}))
 	.pipe(gulp.dest(srcFonts + 'icons/'))
 	.pipe(reload({
-		stream			: true
+		stream: true
 	}));
 });
 
@@ -182,31 +180,31 @@ gulp.task('icons', function() {
 
 gulp.task('js', function() {
 	return gulp.src([
-		srcJS 			+ 'vendors/**/*.js',
-		srcJS 			+ 'scripts.js'
+		srcJS + 'vendors/**/*.js',
+		srcJS + 'scripts.js'
 	])
 	.pipe(plumber({
-		errorHandler	: notify.onError('Error: <%= error.message %>')
+		errorHandler: notify.onError('Error: <%= error.message %>')
 	}))
 	.pipe(concat('scripts.min.js'))
 	.pipe(uglify())
 	.pipe(header(banner, {
-		package			: package
+		package: package
 	}))
 	.pipe(gulp.dest(destJS))
 	.pipe(reload({
-		stream			: true
+		stream: true
 	}));
 });
 
 
 /* ============================== */
-/*  REPLACE
+/*  Replace
 /* ============================== */
 
 gulp.task('replace', function() {
 	return gulp.src([
-		srcTemplates 		+ 'head.html'
+		srcTemplates + 'head.html'
 	])
 	.pipe(replace(/{{JQUERY_VERSION}}/g, bower.devDependencies.jquery))
 	.pipe(gulp.dest(srcTemplates));
@@ -214,27 +212,20 @@ gulp.task('replace', function() {
 
 
 /* ============================== */
-/*  MOVE
+/*  Move
 /* ============================== */
 
 gulp.task('move', function() {
 	return gulp.src([
-		srcRoot 		+ '*.*',
-	    srcRoot			+ 'assets/pdf/*',
-		srcCSS 			+ '*.css',
-		srcFonts 		+ '**/*',
-		srcImg 			+ '**/*',
-	    srcJS 			+ '**/*',
-	    '!' + srcRoot 	+ 'bower.json',
-	    '!' + srcRoot 	+ 'gulpfile.js',
-	    '!' + srcRoot 	+ 'package.json',
-	    '!' + srcRoot 	+ 'README.md',
-	    '!' + srcRoot 	+ 'npm-debug.log',
-	    '!' + srcJS 	+ 'vendors/',
-	    '!' + srcJS 	+ 'vendors/**/*',
-	    '!' + srcJS 	+ 'scripts.js'
+		srcRoot + '*.*',
+		srcFonts + '**/*',
+		srcImg + '**/*',
+	    srcJS + '**/*',
+	    '!' + srcJS + 'vendors/',
+	    '!' + srcJS + 'vendors/**/*',
+	    '!' + srcJS + 'scripts.js'
 	], {
-		base			: srcRoot
+		base: srcRoot
 	})
 	.pipe(changed(destRoot))
 	.pipe(gulp.dest(destRoot));
@@ -242,32 +233,32 @@ gulp.task('move', function() {
 
 
 /* ============================== */
-/*  WEBSERVER
+/*  Webserver
 /* ============================== */
 
 gulp.task('webserver', function() {
 	browserSync.init({
-		proxy			: 'http://192.168.33.10'
+		proxy: 'http://192.168.33.10'
 	});
 });
 
 
 /* ============================== */
-/*  WATCH
+/*  Watch
 /* ============================== */
 
 gulp.task('watch', function() {
-	gulp.watch(srcRoot 	+ '**/*.html', ['html']);
-	gulp.watch(srcCSS 	+ '**/*.scss', ['css']);
-	gulp.watch(srcFonts	+ '**/*', ['fonts']);
-	gulp.watch(srcImg 	+ '*', ['images']);
+	gulp.watch(srcRoot + '**/*.html', ['html']);
+	gulp.watch(srcCSS + '**/*.scss', ['css']);
+	gulp.watch(srcFonts + '**/*', ['fonts']);
+	gulp.watch(srcImg + '*', ['images']);
 	gulp.watch(srcIcons + '*.svg', ['icons']);
-	gulp.watch(srcJS 	+ '**/*.js', ['js']);
+	gulp.watch(srcJS + '**/*.js', ['js']);
 });
 
 
 /* ============================== */
-/*  BUILD
+/*  Build
 /* ============================== */
 
 gulp.task('build', function() {
@@ -284,7 +275,7 @@ gulp.task('build', function() {
 
 
 /* ============================== */
-/*  DEFAULT
+/*  Default
 /* ============================== */
 
 gulp.task('default', function() {
